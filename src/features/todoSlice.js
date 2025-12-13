69453f276c75b5c90321aca6c651dd44499787fd
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-12-13 10:49:30 +0500

    feat: integrate Redux for state management and add todoSlice
    - Added Redux store configuration in src/app/store.js
    - Created todoSlice in src/features/todoSlice.js for managing todo state
    - Updated package.json to include necessary dependencies for Redux and Material UI
    - Modified main.jsx to wrap the application with Redux Provider and BrowserRouter
    - Updated index.css to include Tailwind CSS and Flowbite styles
    - Adjusted vite.config.js to include Tailwind CSS and Flowbite plugins

---
+++ src/features/todoSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todo: [
+        {
+            id: 1,
+            text: "hello"
+        }
+    ]
+};
+
+const todoSlice = createSlice({
+    name: "todo",
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todos = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todo.push(todos)
+        },
+
+        removeTodo: (state, action) => {
+            state.todo = state.todo.filter(todo => todo.id !== action.payload)
+        },
+
+        editTodo: (state, action) => { 
+            // state.todo = state.todo.filter(todo => todo.id !== action.payload)
+         }
+    }
+})
+
+export default todoSlice.reducer;
+export const { addTodo, removeTodo, editTodo } = todoSlice.actions;
